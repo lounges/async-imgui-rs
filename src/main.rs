72d024ff4fab9377e60e8218573aef955779981e
Joshua Martin <josh@hive05.com>
2020-01-01 19:44:59 -0800

    cleanup/comments

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,31 @@
 use async_std::{prelude::*, task};
-use std::time::Duration;
+use futures::channel::mpsc;
+use futures::sink::SinkExt;
+
+use std::time::{Duration, Instant};
 
 use glium::glutin::{self, Event, WindowEvent};
 use glium::{Display, Surface};
+
 use imgui::*;
 use imgui_glium_renderer::Renderer;
 use imgui_winit_support::{HiDpiMode, WinitPlatform};
-use std::time::Instant;
 
-use futures::channel::mpsc;
-use futures::sink::SinkExt;
 type Sender<T> = mpsc::UnboundedSender<T>;
 type Receiver<T> = mpsc::UnboundedReceiver<T>;
 
 #[derive(Debug)]
 enum UiEvent {
     ToggleUiState { current_state: bool },
-}
-
-#[derive(Debug)]
-enum UiEventCompleted {
-    ConsoleMessageFinished { new_state: bool },
+    ToggleUiStateFinished { new_state: bool },
 }
 
 fn main() {
     task::block_on(run_ui());
 }
 
-async fn broker_loop(mut inbound_events: Receiver<UiEvent>, mut outbound_events: Sender<UiEventCompleted>) {
+//background task which manages processing messages independently of the GUI
+async fn broker_loop(mut inbound_events: Receiver<UiEvent>, mut outbound_events: Sender<UiEvent>) {
     while let Some(event) = inbound_events.next().await {
         match event {
             UiEvent::ToggleUiState { current_state } => {
@@ -35,20 +33,26 @@ async fn broker_loop(mut inbound_events: Receiver<UiEvent>, mut outbound_events:
                 println!("Toggling button state in {} seconds...", sleep_duration);
                 task::sleep(Duration::from_secs(sleep_duration)).await;
                 println!("Changing now!");
-                outbound_events.send(UiEventCompleted::ConsoleMessageFinished { new_state: !current_state }).await.unwrap();
+                outbound_events.send(UiEvent::ToggleUiStateFinished { new_state: !current_state }).await.unwrap();
             }
+            _ => (),
         }
     }
 }
 
 async fn run_ui() {
+    //setup two channels and a broker
+    //this gives us a bi-directional channel we can use to communicate between the UI and
+    //any background activity
     let (mut broker_sender, broker_receiver) = mpsc::unbounded();
     let (ui_sender, mut ui_receiver) = mpsc::unbounded();
+    //setup the broker task, it is responsible for performing actions in the background without blocking the GUI
     let _broker_handle = task::spawn(broker_loop(broker_receiver, ui_sender));
 
+    //setup our imgui drawing ccontext
     let mut imgui = Context::create();
-    imgui.set_ini_filename(None);
 
+    //setup window/imgui renderer
     let title = "async-imgui";
     let mut events_loop = glutin::EventsLoop::new();
     let context = glutin::ContextBuilder::new().with_vsync(true);
@@ -56,21 +60,28 @@ async fn run_ui() {
         .with_title(title.to_owned())
         .with_dimensions(glutin::dpi::LogicalSize::new(1024f64, 768f64));
     let display = Display::new(builder, context, &events_loop).expect("Failed to initialize display");
+    let mut renderer = Renderer::init(&mut imgui, &display).expect("Failed to initialize renderer");
 
-    let mut platform = WinitPlatform::init(&mut imgui);
+    //bind the platform events to imgui
     let gl_window = display.gl_window();
     let window = gl_window.window();
+    let mut platform = WinitPlatform::init(&mut imgui);
     platform.attach_window(imgui.io_mut(), &window, HiDpiMode::Rounded);
 
-    let mut renderer = Renderer::init(&mut imgui, &display).expect("Failed to initialize renderer");
-    let mut last_frame = Instant::now();
-    let mut run = true;
+    //window state
     let mut show_extra_label = true;
 
+    //run state
+    let mut run = true;
+    let mut last_frame = Instant::now();
+
     while run {
+        //handle platform events first
         events_loop.poll_events(|event| {
+            //pass to imgui
             platform.handle_event(imgui.io_mut(), &window, &event);
 
+            //handle close manually
             if let Event::WindowEvent { event, .. } = event {
                 if let WindowEvent::CloseRequested = event {
                     run = false;
@@ -78,29 +89,33 @@ async fn run_ui() {
             }
         });
 
+        //poll here instead of await so we do not block the gui thread
         let status = futures::poll!(ui_receiver.next());
         match status {
             futures::task::Poll::Ready(message) => {
                 let message = message.unwrap();
                 println!("Some message is here: {:?}", message);
                 match message {
-                    UiEventCompleted::ConsoleMessageFinished { new_state } => {
+                    UiEvent::ToggleUiStateFinished { new_state } => {
                         show_extra_label = new_state;
                     }
+                    _ => (),
                 }
             }
             _ => (),
         };
 
-        //handle io
+        //prep a new frame
         let io = imgui.io_mut();
         platform.prepare_frame(io, &window).expect("Failed to start frame");
         last_frame = io.update_delta_time(last_frame);
 
         //draw gui
         let ui = imgui.frame();
-        Window::new(im_str!("async imgui-rs")).size([400.0, 300.0], Condition::FirstUseEver).build(&ui, || {
-            ui.text_wrapped(im_str!("Click the button below to toggle the text below after some time."));
+        Window::new(im_str!("async imgui-rs")).size([300.0, 300.0], Condition::FirstUseEver).build(&ui, || {
+            ui.text_wrapped(im_str!(
+                "Click the button below to toggle the text below after some time.  You should still be able to drag this window while that is happening."
+            ));
             if show_extra_label {
                 ui.text_wrapped(im_str!("This line is extra!"));
             }
